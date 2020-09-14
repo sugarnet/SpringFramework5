02cb26f5a56499207f7ec78c33a65b64a350c27a
Diego Scifo <diego.scifo@globallogic.com>
2020-09-14 08:12:29 -0300

    Finish pdf export and excel export. Start csv export.

---
+++ spring-boot-data-jpa/src/main/java/com/dss/data/jpa/app/view/csv/CustomersCsvView.java
@@ -0,0 +1,53 @@
+package com.dss.data.jpa.app.view.csv;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.AbstractView;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
+
+import com.dss.data.jpa.app.entity.Customer;
+
+@Component("modules/customer/list")
+public class CustomersCsvView extends AbstractView {
+	
+	public CustomersCsvView() {
+		setContentType("text/csv");
+	}
+
+	@Override
+	protected boolean generatesDownloadContent() {
+		return true;
+	}
+
+	@Override
+	protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		
+		MessageSourceAccessor messageSourceAccessor = getMessageSourceAccessor();
+		
+		response.setHeader("Content-Disposition", "attachment; filename=\"" + messageSourceAccessor.getMessage("text.customer.list.title") + ".csv\"");
+		response.setContentType(getContentType());
+		
+		Page<Customer> customers = (Page<Customer>) model.get("customers");
+		
+		ICsvBeanWriter beanWriter = new CsvBeanWriter(response.getWriter(), CsvPreference.STANDARD_PREFERENCE);
+		
+		String header[] = {"id", "name", "lastname", "email", "createdAt"};
+		
+		for (Customer customer : customers) {
+			beanWriter.write(customer, header);
+		}
+		
+		beanWriter.close();
+		
+	}
+
+}
--- spring-boot-data-jpa/src/main/java/com/dss/data/jpa/app/view/pdf/BillPdfView.java
+++ spring-boot-data-jpa/src/main/java/com/dss/data/jpa/app/view/pdf/BillPdfView.java
@@ -1,11 +1,17 @@
 package com.dss.data.jpa.app.view.pdf;
 
+import java.awt.Color;
+import java.util.Locale;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.view.document.AbstractPdfView;
 
 import com.dss.data.jpa.app.entity.Bill;
@@ -19,6 +25,17 @@ import com.lowagie.text.pdf.PdfWriter;
 @Component("modules/bill/details")
 public class BillPdfView extends AbstractPdfView {
 
+	/*
+	 * https://itextpdf.com/es/resources/api-documentation
+	 */
+	
+	@Autowired
+	private MessageSource messageSource;
+	
+	@Autowired 
+	private LocaleResolver localeResolver;
+	
+	
 	@Override
 	protected void buildPdfDocument(Map<String, Object> model, Document document, PdfWriter writer,
 			HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -27,34 +44,54 @@ public class BillPdfView extends AbstractPdfView {
 		
 		PdfPTable customerTable = new PdfPTable(1);
 		customerTable.setSpacingAfter(20);
-		customerTable.addCell("Customer Data");
+		
+		Locale locale = localeResolver.resolveLocale(request);
+		
+		// using MessageSourceAccesor is better. Because this object uses localeResolver and messageSource
+		MessageSourceAccessor messageSourceAccessor = getMessageSourceAccessor();
+		
+		
+		PdfPCell cell = new PdfPCell(new Phrase(messageSource.getMessage("text.customer.details.customer.data", null, locale)));
+		cell.setBackgroundColor(new Color(184, 218, 255));
+		cell.setPadding(8f);
+		
+		customerTable.addCell(cell);
 		customerTable.addCell(bill.getCustomer().getName().concat(" ").concat(bill.getCustomer().getLastname()));
 		customerTable.addCell(bill.getCustomer().getEmail());
 		
 		PdfPTable billTable = new PdfPTable(1);
 		billTable.setSpacingAfter(20);
-		billTable.addCell("Bill Data");
-		billTable.addCell("Nro: ".toString().concat(bill.getId().toString()));
-		billTable.addCell("Description: ".concat(bill.getDescription()));
-		billTable.addCell("Date: ".concat(bill.getCreatedAt().toString()));
+		
+		new PdfPCell(new Phrase(messageSource.getMessage("text.bill.details.bill.data", null, locale)));
+		cell.setBackgroundColor(new Color(195, 230, 203));
+		cell.setPadding(8f);
+		
+		billTable.addCell(cell);
+		billTable.addCell(messageSourceAccessor.getMessage("text.bill.details.nro").concat(": ").concat(bill.getId().toString()));
+		billTable.addCell(messageSourceAccessor.getMessage("text.bill.details.description").concat(": ").concat(bill.getDescription()));
+		billTable.addCell(messageSourceAccessor.getMessage("text.bill.details.date").concat(": ").concat(bill.getCreatedAt().toString()));
 		
 		document.add(customerTable);
 		document.add(billTable);
 		
 		PdfPTable billItemTable = new PdfPTable(4);
-		billItemTable.addCell("Description");
-		billItemTable.addCell("Quantity");
-		billItemTable.addCell("Price");
-		billItemTable.addCell("Total");
+		billItemTable.setWidths(new float[] {3.5f, 1, 1, 1});
+		billItemTable.addCell(messageSourceAccessor.getMessage("text.bill.details.bill.item.description"));
+		billItemTable.addCell(messageSourceAccessor.getMessage("text.bill.details.bill.item.quantity"));
+		billItemTable.addCell(messageSourceAccessor.getMessage("text.bill.details.bill.item.price"));
+		billItemTable.addCell(messageSourceAccessor.getMessage("text.bill.details.bill.item.total"));
 		
 		for(BillItem bi : bill.getItems()) {
 			billItemTable.addCell(bi.getProduct().getDescription());
-			billItemTable.addCell(bi.getQuantity().toString());
+			
+			cell = new PdfPCell(new Phrase(bi.getQuantity().toString()));
+			cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+			billItemTable.addCell(cell);
 			billItemTable.addCell(bi.getProduct().getPrice().toString());
 			billItemTable.addCell(bi.getSubtotal().toString());
 		}
 		
-		PdfPCell cell = new PdfPCell(new Phrase("Total"));
+		cell = new PdfPCell(new Phrase(messageSourceAccessor.getMessage("text.bill.details.total")));
 		cell.setColspan(3);
 		cell.setHorizontalAlignment(PdfPCell.ALIGN_RIGHT);
 		billItemTable.addCell(cell);
